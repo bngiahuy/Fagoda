f2568e0fa394f7d935b27fa5ec9a687b40d8a996
Nguyễn Nhật Anh <ace2002luffy@gmail.com>
2022-11-03 15:06:05 +0700

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-// import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 const App = () => {
   return (
     <div>
-      {/* <Routes>
-        <Route path="/" element={<Navigate to="/Home" replace />} />
-        <Route path="/Home" element={<Home />}/>
-      </Routes> */}
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />}></Route>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Header from '../../components/Header';
 
 const Home = () => {
 
   return (
     <div>
-      <Header/>
+      <p>This is homepage</p>
     </div>
   );
 }

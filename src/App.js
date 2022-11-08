420cc5393b413530b4a9759a2d78f47c9e546b3c
bngiahuy <45910202+bngiahuy@users.noreply.github.com>
2022-11-08 21:44:20 +0700

    update Profile.js
    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Navigate to="/home" replace />}></Route>
           <Route path="/admin" element={<Admin />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile/>} />
         </Routes>
       </BrowserRouter>
     </div>

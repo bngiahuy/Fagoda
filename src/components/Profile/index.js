a490040dd9c3620fa43b32a93b6e30c59ba53519
bngiahuy <45910202+bngiahuy@users.noreply.github.com>
2022-11-11 00:49:28 +0700

    .

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,14 +3,19 @@ import './Profile.css'
 import { RightTab } from '../../components/Home/RightTab';
 import { LeftTab } from '../../components/Home/LeftTab';
 import CoverKhachHang from 'assets/Profile/AnhBiaKhachHang.jpg'
+import avatar from 'assets/Profile/avatar.jpg'
 export default function Profile() {
   return (
     <div className='profile'>
       <LeftTab />
       <div className='content'>
-        
           <img className='cover-img' src={CoverKhachHang}></img>
-        
+          <div idName='avatar-circle'>
+          <img className='avatar' src={avatar}></img>
+
+          </div>
+
+          
       </div>
       <RightTab />
     </div>
